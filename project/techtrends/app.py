b885f6af819f7db628a98ef5986c15c67ef1935e
Rakesh PD <rakesh.damodaran@hungerstation.com>
2022-11-05 11:37:39 +0400

    feat(containarization): modified the code

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,25 +1,33 @@
 import sqlite3
-
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    app.config['active_connection_count'] = app.config['active_connection_count'] + 1
     return connection
 
+ # Function to close a database connection
+def close_db_connection(connection):
+    connection.close()
+    app.config['active_connection_count'] = app.config['active_connection_count'] - 1
+    
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
-    connection.close()
+    close_db_connection(connection)
     return post
 
 # Define the Flask application
 app = Flask(__name__)
+app.config['active_connection_count'] = 0
+logging.basicConfig(level=logging.DEBUG)
 app.config['SECRET_KEY'] = 'your secret key'
 
 # Define the main route of the web application 
@@ -27,7 +35,7 @@ app.config['SECRET_KEY'] = 'your secret key'
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
+    close_db_connection(connection)
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -38,12 +46,37 @@ def post(post_id):
     if post is None:
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "'+ post['title'] +'" retrieved!');
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Healthcheck
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response 
+
+# Healthcheck
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    results = connection.execute('SELECT * FROM posts').fetchall()
+    response = app.response_class(
+        response=json.dumps({"db_connection_count": app.config['active_connection_count'], "post_count": len(results)}),
+        status=200,
+        mimetype='application/json'
+    )
+    close_db_connection(connection)
+    return response 
+    
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
